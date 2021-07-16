9064542fc3d969757859aff2f4ebb94de12c9496
okitomonaga <okinaga.tomohiro@learning-engineering.com>
2021-07-16 06:59:50 +0000

    平行移動テストケース２作成

--- application_test.go
+++ application_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	//"reflect"
-	"fmt"
+	//"fmt"
 	"gonum.org/v1/gonum/mat"
 	"testing"
 )
@@ -11,10 +11,18 @@ func TestParallelTp1(t *testing.T) {
 	x := []float64{1, 2, 1}
 	operation_target := mat.NewDense(3, 1, x)
 	result := parallelTp(operation_target, 1, 1)
-	fa := mat.Formatted(result, mat.Prefix(""), mat.Squeeze())
-	fmt.Printf("%v\n", fa)
 
 	if result.At(0, 0) != 2 || result.At(1, 0) != 3 {
 		t.Error("ParallelTp1 is failed")
+	}
+}
+
+func TestParallelTp2(t *testing.T) {
+	x := []float64{3, 3, 1}
+	operation_target := mat.NewDense(3, 1, x)
+	result := parallelTp(operation_target, 1, 1)
+
+	if result.At(0, 0) != 4 || result.At(1, 0) != 4 {
+		t.Error("ParallelTp2 is failed")
 	}
 }
