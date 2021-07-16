3edf2b64e7347ed354d4902ba5bba31c091d646f
okitomonaga <okinaga.tomohiro@learning-engineering.com>
2021-07-16 06:09:04 +0000

    平行移動のテストケース作成

--- application_test.go
+++ application_test.go
@@ -5,3 +5,10 @@ import (
 	//"fmt"
 	"testing"
 )
+
+func TestParallelTp1(t *testing.T) {
+	result := ParallelTp("1,2")
+	if result[0] != 1 || result[1] != 2 { //resultと"hoge"の型を比較している
+		t.Error("ParallelTp1 is failed")
+	}
+}
